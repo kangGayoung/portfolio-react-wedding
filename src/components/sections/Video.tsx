005b26a9e874d6a9da0a7f3ab6ada1e2f5b67a86
kangGayoung <gangga9011@gmail.com>
2024-03-13 15:08:27 +0900

    사진첩 부분까지 완성

---
+++ src/components/sections/Video.tsx
@@ -0,0 +1,24 @@
+import classNames from 'classnames/bind'
+import styles from './Video.module.scss'
+import Section from '@shared/Section'
+
+const cx = classNames.bind(styles)
+
+function Video() {
+  return (
+    <Section className={cx('container')}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        controls={true}
+        // poster -> 썸네일
+        poster="/assets/poster.jpg"
+      >
+        <source src="/assets/main.mp4" type="video/mp4"></source>
+      </video>
+    </Section>
+  )
+}
+
+export default Video
